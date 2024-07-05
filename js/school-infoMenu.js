893094e5f818bc26581d00189b3f8e50295e057b
Julia Bragina <triulya@yandex.ru>
2024-07-05 10:32:46 +0000

    изменены стили для меню

--- js/school-infoMenu.js
+++ js/school-infoMenu.js
@@ -4,10 +4,12 @@ document.addEventListener("DOMContentLoaded", function() {
   const buttonImg = document.querySelector('.school-info__buttonImg');
   const links = document.querySelectorAll('.school-info__item');
   const activeItem = document.querySelector('.school-info__activeItem');
+  const mainContent = document.querySelector('.mainInfo');
     
   button.addEventListener('click', function() { 
     items[1].classList.toggle('school-info__items_is_active');
     buttonImg.classList.toggle('school-info__buttonImg_is_active');
+    mainContent.classList.toggle('mainInfo_is_menuOpened');
   });
   
   links.forEach(link => {
